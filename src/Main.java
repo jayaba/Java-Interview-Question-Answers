c9f6d5a1975d471752061ddbcc3cafc53cb20ca1
jayaba <erangajayaba0@gmail.com>
2022-10-04 19:35:02 +0530

    find large/small number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        // 1] Java code to find Smallest and Largest Element in an Array
+
+        int[] arr = {32,46,1,32,45,23,45,121,34,277,4,8};
+
+        int largestNum =arr[0];
+        int smallestNum=arr[0];
+
+      for(int i=0;i<arr.length-1;i++){
+          if (arr[i]>largestNum){
+              largestNum = arr[i];
+
+          } if (arr[i]<smallestNum){
+              smallestNum = arr[i];
+          }
+
+      }System.out.println("largest Number is - " + largestNum);
+        System.out.println("smallest value is - " + smallestNum);
+
+
+
     }
 }
